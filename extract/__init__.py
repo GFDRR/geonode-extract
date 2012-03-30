137c5248cc6241fde9196a3a2452685211229152
Ariel Núñez <ingenieroariel@gmail.com>
2012-03-30 12:05:41 -0400

    Fixed problem in extract version

--- extract/__init__.py
+++ extract/__init__.py
@@ -1,6 +1,6 @@
 # Make the version available if there is a version.py file
 try:
-    from safe.version import git_revision as __git_revision__
+    from extract.version import git_revision as __git_revision__
 except ImportError:
     __version__ = "unknwon"
     __git_revision__ = None
