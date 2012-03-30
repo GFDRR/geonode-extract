4e3439350f721829688c3501cb702bb2e1808397
Ariel Núñez <ingenieroariel@gmail.com>
2012-03-30 12:06:59 -0400

    Added shortcut to __version__

--- extract/__init__.py
+++ extract/__init__.py
@@ -1,5 +1,6 @@
 # Make the version available if there is a version.py file
 try:
+    from extract.version import version as __version__
     from extract.version import git_revision as __git_revision__
 except ImportError:
     __version__ = "unknwon"
