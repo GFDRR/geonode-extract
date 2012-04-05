823224a89ab653a8188767b28a4d59ff3e087470
Ariel Núñez <ingenieroariel@gmail.com>
2012-04-05 10:58:38 -0400

    0.2.7-dev

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ def read(fname):
 
 MAJOR = 0
 MINOR = 2
-MICRO = 6
-ISRELEASED = True
+MICRO = 7
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 # Return the git revision as a string
